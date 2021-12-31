9b4adee85582f9e85e112142d89c6c22e11950ce
Dominik <dominik.tomasi@gmail.com>
2021-12-31 09:39:33 +0100

    chore: squash this later

--- lib/utils.bash
+++ lib/utils.bash
@@ -84,6 +84,7 @@ install_version() {
     mkdir -p "$install_path"
     echo "* Moving downloads to $install_path"
     cp -r "$ASDF_DOWNLOAD_PATH"/* "$install_path"
+    ls -la "$install_path"
 
     local tool_cmd
     tool_cmd="$(echo "$TOOL_TEST" | cut -d' ' -f1)"
